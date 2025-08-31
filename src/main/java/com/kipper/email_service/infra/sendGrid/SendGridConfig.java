0b1cd9a4ce0e49cccb80b9de60469ffcdd739f66
Lipe-lisboa <filipelisboaalves@gmail.com>
2025-08-31 19:58:09 -0300

    Mudando o nome da variavel de ambiente, e criando um arquivo .env de exemplo

--- src/main/java/com/kipper/email_service/infra/sendGrid/SendGridConfig.java
+++ src/main/java/com/kipper/email_service/infra/sendGrid/SendGridConfig.java
@@ -1,6 +1,7 @@
 package com.kipper.email_service.infra.sendGrid;
 
 import com.sendgrid.SendGrid;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +9,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SendGridConfig {
 
-    @Value("${API_KEY}")
-    private String apiKey;
+    //@Value("${API_KEY}")
+    //private String apiKey;
 
     @Bean
     public SendGrid sendGrid() {
-        return new SendGrid(apiKey);
+        Dotenv dotenv = Dotenv.load();
+        return new SendGrid(dotenv.get("API_SEND_GRID_KEY"));
     }
 }
